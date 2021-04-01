17a09b978e01e969ae9faf60ccc2d2c53260400c
Felipe <felipee.trova@gmail.com>
2021-03-31 22:39:02 -0300

    Add initial base project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '25147b33d48972890e465ef7b17ab815'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f149c02c-e9f3-412a-8243-9b1566fa4740'
+};
